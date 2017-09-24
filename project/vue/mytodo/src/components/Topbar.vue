ba36ed409ebb92431df05cac6a0035fdbd2e1e3b
chenyu <chenyuhero@hotmail.com>
2017-09-24 21:13:38 +0800

    登录框
    完成了登录和注册

--- project/vue/mytodo/src/components/Topbar.vue
+++ project/vue/mytodo/src/components/Topbar.vue
@@ -2,33 +2,77 @@
 	<div id="topbar">
 
 		<div class="wrapper">
-			<span class="logo">Todo</span>
+			<span class="logo">Todo
+				<svg class="icon" aria-hidden="true">
+				<use xlink:href="#icon-dui"></use>
+				</svg>
+			</span>
 			<div class="actions">
-				<el-button>登录</el-button>
-				<el-button type="primary">注册</el-button>
+				<el-button @click="login"  >登录</el-button>
+				<el-button @click="regist" type="primary">注册</el-button>
 				
 			</div>
-		
 		</div>
-			<div class="regist">
 
-			  
-                <header>
-                    <span class="close">×</span>
-                    <h3>请登录</h3>
-                </header>
-                <main>
-                    <el-input v-model="input" placeholder="邮箱"></el-input>
-                    <el-input v-model="input" placeholder="密码"></el-input>
+		<div v-bind:class="{show : loginr}" >
+			<div id="regist" >
+			  	<div class="panel">
+			  	
+			  		<header>
+                    	<span class="close" @click="close">×</span>
+                   		<h3>现在Todo
+                    		<svg class="icon" aria-hidden="true">
+    							<use xlink:href="#icon-dui"></use>
+							</svg>
+						</h3>
+            	    </header>
+                	<main>
+	                    <el-input class="input"  placeholder="邮箱"></el-input>
+	                    <el-input class="input"  placeholder="密码"></el-input>
+	                    <el-input class="input"  placeholder="确认密码"></el-input>
+                	</main>
+	                <footer>
+	                	<div>
+	                		<el-button type="primary">提交注册</el-button>
+	                	</div>
+							
+	                </footer>
+        		</div>
+		    </div>		
+       </div>		
 
-                </main>
-                <footer>
-                	<el-button>登录</el-button>
-                </footer>
+		<div v-bind:class="{hide : hidel}">
+			<div id="login"  >
+			  	<div class="panel">
+			  	  	<header>
+	                    <span class="close" @click="close">×</span>
+	                    <h3>现在Todo
+	                    	<svg class="icon" aria-hidden="true">
+	    					<use xlink:href="#icon-dui"></use>
+							</svg>
+						</h3>
+	                </header>
+	                <main>
+	                    <el-input class="input"  placeholder="邮箱"></el-input>
+	                    <el-input class="input"  placeholder="密码"></el-input>
+
+	                </main>
+	                <footer>
+	                	<div>
+	                		<el-button type="primary">登录</el-button>
+	                	</div>
+						<div>
+							<el-button type="text">忘记密码</el-button>
+							<el-button type="text">没有账号</el-button>
+						</div>
+					</footer>
+			 	</div>
+            </div>
+		</div>
              
-        </div>		
+		
 
-
+        
 	</div>
 
 
@@ -38,21 +82,39 @@
   export default {
     data() {
       return {
-        form: {
-          mail: '',
-          password: '',
-          }
-      }
+      	loginr : false ,
+      	hidel : false ,
+       }
     },
     methods: {
       onSubmit() {
         console.log('submit!');
+      },
+      login(){
+      	this.hidel = true;
+      	console.log("显示了登录")
+      },
+      regist(){
+      	this.loginr = true;
+      	console.log("显示了注册")
+      },
+      close(){
+      	this.loginr = false;
+      	this.hidel = false;
+      	console.log("关闭")
       }
     }
   }
 </script>
 <style scoped >
+	
+	.show #regist{
+		display: block;
+	}
 
+	.hide #login{
+		display: block;
+	}
 	#topbar{
 	background: #ffffff;
 	box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
@@ -76,14 +138,66 @@
 	color:#000000;
 	}
 	
-	.regist{
-		border: 1px solid red;
-		height: 300px;
-		width: 600px;
+	#regist {
+		position: fixed;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.4);
+
+
+	}
+	 #login{
+		position: fixed;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.4);
+        
 	}
 	
-	.panel main {
-            padding: 16px;
-    }
+	.panel{
+		border-radius: 5px;
+		border-radius: 5px;
+		position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 400px;
+        height: 300px;
+        transform: translate(-50%, -50%);
+        background-color: #fff;
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	}
 
+	 .panel header{
+	    background-color: #009688;
+	    padding: 1px 16px;
+	    color: #fff;
+	}
+
+ 	 .panel .close {
+	    float: right;
+	    margin-top: 16px;
+	    font-size: 20px;
+	    cursor: pointer;
+	}
+
+	 .panel main {
+	    padding: 16px;
+	    
+	}
+	
+	 .panel main .input{
+		margin: 8px auto;
+	}
+
+	#regist{
+		display: none;
+	}
+	#login{
+		display: none;
+	}
+	
 </style>
