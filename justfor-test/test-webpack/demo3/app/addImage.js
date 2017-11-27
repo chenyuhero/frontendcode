a139a1c6a3ae0032a8dd87fe815115e6ffea264e
chenyuhero <chenyuhero@hotmail.com>
2017-11-27 16:31:53 +0800

    pic

---
+++ justfor-test/test-webpack/demo3/app/addImage.js
@@ -0,0 +1,7 @@
+let smallImage = document.createElement('img')
+smallImage.src = require('../images/small.jpeg')
+document.body.appendChild(smallImage)
+
+let bigImage = document.createElement('img')
+bigImage.src = require('../images/big.jpeg')
+document.body.appendChild(bigImage)
---
+++ justfor-test/test-webpack/demo3/app/sum.js
@@ -0,0 +1,11 @@
+
+
+/*
+module.exports = function(a,b){
+    return a + b
+}
+//传统写法
+*/
+export default(a,b) =>{
+    return a + b
+}
