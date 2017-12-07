a2646c9146ba112bfe2a271b6b4b0664d0b71c2f
chenyuhero <chenyuhero@hotmail.com>
2017-12-07 21:06:44 +0800

    done

--- mvc-test/src/moudle3.js
+++ mvc-test/src/moudle3.js
@@ -6,3 +6,28 @@ $module3.on("change","input",function(){
 $module3.on("click","button",function(){
     console.log("click3")
 })
+
+function Maker(options){
+    let controler = {}
+    controler.$element = $(options.element)
+    controler.$events = options.events
+
+    for(key in controler.$events){
+        let parts = key.split[' ']
+        let envntType = parts.shift()
+        let selector = parts.join('')
+        controler.$element.on(enventType,selector,controler.$events[key])
+    }
+}
+
+Maker({
+    element:".module3",
+    events:{
+        'change ,input':function(e){
+            console.log('change3')
+        },
+        'click button':function(e){
+            console.log('click3')
+        }
+    }
+})
