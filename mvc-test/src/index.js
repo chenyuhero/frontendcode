74085b94927cbfbd9ddecc5e5b0d39cc1220d984
chenyuhero <chenyuhero@hotmail.com>
2017-12-07 16:15:41 +0800

    v1

--- mvc-test/src/index.js
+++ mvc-test/src/index.js
@@ -1,1 +1,26 @@
 import "../style/border.css"
+import $ from "jquery"
+
+let $module1 = $(".module1")
+$module1.on("change","input",function(){
+    console.log("chang1")
+})
+$module1.on("click","button",function(){
+    console.log("click1")
+})
+
+let $module2 = $(".module2")
+$module2.on("change","input",function(){
+    console.log("change2")
+})
+$module2.on("click","button",function(){
+    console.log("click2")
+})
+
+let $module3 = $(".module3")
+$module3.on("change","input",function(){
+    console.log("3")
+})
+$module3.on("click","button",function(){
+    console.log("click3")
+})
