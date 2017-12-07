ae54bf7e1a3c8bdb906a70200bf3b554b833189b
chenyuhero <chenyuhero@hotmail.com>
2017-12-07 21:24:39 +0800

    fengzhuang

--- mvc-test/src/moudle3.js
+++ mvc-test/src/moudle3.js
@@ -1,29 +1,10 @@
 import $ from "jquery"
-let $module3 = $(".module3")
-$module3.on("change","input",function(){
-    console.log("3")
-})
-$module3.on("click","button",function(){
-    console.log("click3")
-})
+import Controler from './Controler.js'
 
-function Maker(options){
-    let controler = {}
-    controler.$element = $(options.element)
-    controler.$events = options.events
-
-    for(key in controler.$events){
-        let parts = key.split[' ']
-        let envntType = parts.shift()
-        let selector = parts.join('')
-        controler.$element.on(enventType,selector,controler.$events[key])
-    }
-}
-
-Maker({
+new Controler({
     element:".module3",
     events:{
-        'change ,input':function(e){
+        'change input':function(e){
             console.log('change3')
         },
         'click button':function(e){
